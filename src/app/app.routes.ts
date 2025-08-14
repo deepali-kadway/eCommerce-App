a25a242fe96ad3bac7f093f78402478e7d30cd94
Deepali Kadway <dkadway@gmail.com>
2025-08-13 22:45:25 -0600

    Home & Product-List component built

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { ProductList } from './components/product-list/product-list';
+
+export const routes: Routes = [
+    {path: '', component: Home}
+];
